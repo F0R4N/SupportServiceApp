7841c71a583875a2922d893ae7cb56e02e175990
for4n <for4n@F0R4N>
2026-03-25 10:02:28 +0300

    Reverted the last changes in SQL stuff cause apparently it gives errors that I cba to fix

--- Models/Client.cs
+++ Models/Client.cs
@@ -4,5 +4,8 @@ public class Client
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string? Phone { get; set; }
+    public string? Email { get; set; }
+
     public ICollection<Ticket>? Tickets { get; set; }
 }
--- Models/Equipment.cs
+++ Models/Equipment.cs
@@ -4,5 +4,7 @@ public class Equipment
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string? InventoryNumber { get; set; }
+
     public ICollection<Ticket>? Tickets { get; set; }
 }
